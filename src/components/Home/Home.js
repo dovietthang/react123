2f76b7605e7f6f754eaa0ae53af9929394de7b33
dovietthang <thangdv062@gmail.com>
2023-09-27 15:47:54 +0700

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,34 @@
 import HomeComtent from "./HomeComtent";
 import HomeWrapper from "./HomeWrapper";
-import FooterFullWrapper from "./FooterFullWrapper";
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 // Import tệp CSS
-import "../../Assets/Style/wp-includes/css/dist/block-library/style.min3781.css"; // Import tệp CSS
-import "../../Assets/Style/wp-includes/css/classic-themes.min3781.css"; // Import tệp CSS
-import "../../Assets/Style/wp-content/cache/min/1/wp-content/plugins/contact-form-7/includes/css/styles-584171c5f3615d6e5a70d2dccba64418.css"; // Import tệp CSS
-import "../../Assets/Style/wp-content/plugins/table-of-contents-plus/screen.minb5ce.css"; // Import tệp CSS
+// import "../../Assets/Style/wp-includes/css/dist/block-library/style.min3781.css"; // Import tệp CSS
+// import "../../Assets/Style/wp-includes/css/classic-themes.min3781.css"; // Import tệp CSS
+// import "../../Assets/Style/wp-content/cache/min/1/wp-content/plugins/contact-form-7/includes/css/styles-584171c5f3615d6e5a70d2dccba64418.css"; // Import tệp CSS
+// import "../../Assets/Style/wp-content/plugins/table-of-contents-plus/screen.minb5ce.css"; // Import tệp CSS
 import "../../Assets/Style/wp-content/cache/min/1/wp-content/themes/southteam/style-bda8a3e2a314d486574927ffb77d5bbe.css"; // Import tệp CSS
 import "../../Assets/Style/wp-content/cache/min/1/wp-content/themes/southteam/css/font-awesome-c28910b13683dd0c7dd202fa7208389d.css"; // Import tệp CSS
 import "../../Assets/Style/wp-content/cache/min/1/wp-content/themes/southteam/css/bootstrap-d1881920b8a3112ed740aa678f3b8d6c.css"; // Import tệp CSS
-import "../../Assets/Style/wp-content/cache/min/1/wp-content/themes/southteam/css/aos-4df847f79c24d7a8dd5ed2983dbd3458.css"; // Import tệp CSS
-import "../../Assets/Style/custuer.style.css"; // Import tệp CSS
+// import "../../Assets/Style/wp-content/cache/min/1/wp-content/themes/southteam/css/aos-4df847f79c24d7a8dd5ed2983dbd3458.css"; // Import tệp CSS
+// import "../../Assets/Style/custuer.style.css"; // Import tệp CSS
 
 function Home() {
   useEffect(() => {
+    // Đoạn mã JavaScript của bạn
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/5aa4e7f3d7591465c70874ab/default";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+
     (function (w, d, s, l, i) {
       w[l] = w[l] || [];
       w[l].push({
@@ -37,6 +50,23 @@ function Home() {
     <div id="content" class="site-content">
       <HomeComtent />
       <HomeWrapper />
+      <Helmet>
+        {/* Đoạn mã JavaScript sẽ được thêm vào phần head */}
+        <script type="text/javascript">
+          {`
+            // Đoạn mã JavaScript của bạn
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/5aa4e7f3d7591465c70874ab/default';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </script>
+      </Helmet>
     </div>
   );
 }
--- src/components/Home/HomeComtent.js
+++ src/components/Home/HomeComtent.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import $ from "jquery";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import CSS của AOS
 import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
@@ -7,127 +8,18 @@ import Section4 from "./Section4";
 import Section5 from "./Section5";
 import Section6 from "./Section6";
 import Section7 from "./Section7";
-
+import MySVG from "./MySVG";
 function HomeComtent() {
   useEffect(() => {
-    // Sử dụng jQuery trong useEffect
-    $(window).on("load", function () {
-      $(".dor-preloader").addClass("preloader-dor-animate");
-      setTimeout(function () {
-        $(".dor-preloader").removeClass("preloader-dor-animate");
-      }, 1500);
-      setTimeout(function () {
-        $(".smooth-transition-loader").fadeOut();
-      }, 2000);
+    // Khởi tạo AOS khi component được render
+    AOS.init({
+      once: true,
     });
   }, []);
 
   return (
     <div>
-      <div class="smooth-transition-loader mimic-ajax">
-        <div class="st-loader">
-          <div class="st-loader1">
-            <div class="dor-preloader">
-              <svg
-                x="0px"
-                y="0px"
-                width="400px"
-                height="120px"
-                viewBox="0 0 360 100"
-                enable-background="new 0 0 277.667 119.667"
-              >
-                <path
-                  class="letter-1"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  d="M6,69.3c0,6,2.1,10.9,6.2,15c4.1,4,9.1,5.9,15.1,5.9c6,0,11.1-1.9,15.3-5.8c4.2-3.9,6.3-8.9,6.3-14.9  c0-6.1-2-10.7-5.9-14c-1.7-1.4-6.9-3.9-6.9-3.9l-0.6-0.2c0,0-9-2.9-14.2-5c-4.8-1.9-6.6-3.3-7.6-4.2c-0.6-0.6-1.7-1.5-2.7-2.9  c-3.1-4.2-3.1-8.9-3.1-11c0-1.5,0-5.1,2-9.1c0.3-0.7,1.6-3.3,4.4-5.8c0.9-0.9,2.7-2.4,5.3-3.6c0.8-0.4,4.6-2.2,10-2.2  c1.7,0,5.3,0.1,9.4,2c1,0.5,3.3,1.7,5.7,3.9c2.7,2.6,4,5.3,4.4,6.1c1.7,3.6,1.9,7,1.9,8.9"
-                />
-                <path
-                  class="letter-2"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  d="M137.8,19.8c-8.1-8.1-17.8-12.1-29.1-12.1c-11.4,0-21.1,4.1-29.2,12.2c-8,8.2-12,17.9-12,29.3  c0,11.1,4.1,20.6,12.3,28.6c8.1,7.9,17.7,11.9,28.9,11.9c11.2,0,20.9-4,29-12c8.2-8,12.2-17.6,12.2-28.9S145.9,27.9,137.8,19.8z"
-                />
-                <line
-                  class="line-2"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  x1="84.8"
-                  y1="94.6"
-                  x2="134.7"
-                  y2="3.7"
-                />
-                <path
-                  class="letter-3"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  d="M168.8,3.9v58c0,3,0.2,5.9,0.6,8.6c0.5,3.4,1.4,6.2,2.5,8.2c4.2,7.6,10.6,11.4,19.2,11.4  c8.6,0,15-3.8,19.2-11.4c1.1-2,2-4.8,2.5-8.2c0.4-2.7,0.7-5.6,0.7-8.6v-58"
-                />
-                <line
-                  class="line-3"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  x1="228"
-                  y1="7.7"
-                  x2="282"
-                  y2="7.7"
-                />
-                <line
-                  class="line-2"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  x1="255"
-                  y1="93.7"
-                  x2="255"
-                  y2="7.7"
-                />
-                <path
-                  class="letter-2"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  d="M296,48.2h48"
-                />
-                <line
-                  class="line-3"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  x1="344"
-                  y1="93.7"
-                  x2="344"
-                  y2="3.7"
-                />
-                <line
-                  class="line-2"
-                  fill="none"
-                  stroke="#FFFFFF"
-                  stroke-width="4"
-                  stroke-miterlimit="10"
-                  x1="296"
-                  y1="3.7"
-                  x2="296"
-                  y2="93.7"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MySVG />
       <div class="wrapper" id="home-page-wrapper">
         <div id="resizable-container">
           <Section1 />
